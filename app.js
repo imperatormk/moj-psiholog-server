6ab2e7e2ed9430fdf6200dbf15d711189a155d93
Darko <darko.simonovski@hotmail.com>
2018-06-16 19:37:33 +0200

    Added and implemented OpekTok SDK

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const express = require('express');
-const app = express();
-var http = require('http').Server(app);
-var router = express.Router();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+var http = require('http').Server(app)
+var router = express.Router()
 
-var bodyParser = require('body-parser');
+const apiKey = '46139462' // move from here
 
-// routes
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const OpenTok = require('opentok'),
+    opentok = new OpenTok(apiKey, 'a9c8977bc84eab8b050062d71093851630990aaf')
 
-app.get('/', function(req, res) {
-	res.json({
-      sane: true
-    });
-});
+const bodyParser = require('body-parser')
 
-http.listen(3002, () => console.log('App listening on port 3002!'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get('/session', function(req, res) {
+  opentok.createSession((err, session) => {
+    if (err) return console.log(err)
+  
+    const sessionId = session.sessionId
+    const token = opentok.generateToken(sessionId)
+
+    res.json({
+      apiKey,
+      sessionId,
+      token
+    })
+  })
+})
+
+http.listen(3002, () => console.log('App listening on port 3002!'))
