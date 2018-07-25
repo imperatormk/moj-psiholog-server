73d452f9f169673ca288610fd36af2dd82de17ef
Darko Simonovski <darko.simonovski@hotmail.com>
2018-07-25 02:43:35 +0200

    Added new DB and API functions for blogs

---
+++ controllers/api/blog-controller.js
@@ -0,0 +1,34 @@
+const db = require('../../db')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.controllers.blogs.list()
+	.then(blogs => res.json(blogs))
+	.catch(err => res.status(500).send(err))
+})
+
+router.post('/', (req, res) => {
+  const blogObj = req.body.blog
+  const userId = req.body.userId
+  
+  if (!blogObj || userId == null) {
+  	res.status(400).send({ msg: 'invalidData' })
+  }
+
+  db.controllers.blogs.create(blogObj, userId)
+	.then((resp) => {
+  	  res.status(200).send({ success: true, data: resp })
+  	})
+	.catch(err => {
+  	  res.status(500).send({ success: false, err })
+  	})
+})
+
+router.delete('/', (req, res) => {
+  db.controllers.blogs.deleteAll()
+	.then(resp => res.status(200).send({ success: true }))
+	.catch(err => res.status(500).send({ success: false, err }))
+})
+
+module.exports = router
